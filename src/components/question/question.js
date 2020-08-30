b7ca805005d0415c51b6608f6925291977e72aea
Leonid Petrov <leonid.petrov@latoken.com>
2020-08-30 20:41:10 +0300

    feat: name is stars when unknown

--- src/components/question/question.js
+++ src/components/question/question.js
@@ -6,7 +6,7 @@ const Question = (props) => {
     const { answer, isRight } = props
     return (
         <div>
-            Name: {isRight ? answer.name : ''}
+            Name: {isRight ? answer.name : '*****'}
             <img src={isRight ? answer.image : ''} alt="" />
             <AudioPlayer
                 src={answer.audio}
